75620471b6f7309a0a43b9644bb11520258494a1
thelastinuit <email+919@aloui.se>
2023-06-02 16:46:44 -0600

    Submillisecond Liveview Rust example

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use serde::{Deserialize, Serialize};
+use submillisecond::{router, static_router, Application};
+use submillisecond_live_view::prelude::*;
+
+#[derive(Clone, Serialize, Deserialize)]
+struct Counter {
+    count: i32,
+}
+
+impl LiveView for Counter {
+    type Events = (Increment, Decrement);
+
+    fn mount(_uri: Uri, _socket: Option<Socket>) -> Self {
+        Counter { count: 0 }
+    }
+
+    fn render(&self) -> Rendered {
+        html! {
+            button @click=(Increment) { "Increment" }
+            button @click=(Decrement) { "Decrement" }
+            p { "Count is " (self.count) }
+            @if self.count >= 5 {
+                p { "Count is high!" }
+            }
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct Increment {}
+
+impl LiveViewEvent<Increment> for Counter {
+    fn handle(state: &mut Self, _event: Increment) {
+        state.count += 1;
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct Decrement {}
+
+impl LiveViewEvent<Decrement> for Counter {
+    fn handle(state: &mut Self, _event: Decrement) {
+        state.count -= 1;
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    Application::new(router! {
+        GET "/" => Counter::handler("src/main.html", "#app")
+        "/static" => static_router!("./static")
+    })
+    .serve("0.0.0.0:3000")
+}
